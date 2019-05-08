825cf45f0561cda52cb86b030c8fc04a08a019ac
Nicklas Askjær <s184199@student.dtu.dk>
2019-05-08 14:21:09 +0200

    Ny og forbedret UI
    By CP and NIC

--- Projectmanager/src/app/UI.java
+++ Projectmanager/src/app/UI.java
@@ -1,21 +1,15 @@
 package app;
 
-import javax.sound.sampled.SourceDataLine;
-
-// Case 1 is Login Screen
-// Case 2 is General Overview
-// Case 3 is Project Overview
-// Case 4 is Create Project
-// Case 5 is Add New Developer to System
-// Case # is Go to project "..."
+import sun.net.www.content.text.plain;
 
 public class UI {
 
     private String activeProject;
     private String activeActivity;
+    private String userID;
 
-    void screen(int screen){
-        switch (screen){
+    void loginScreen(int screen){
+        switch(screen){
             case 1: //Login
                 System.out.println("Please enter your login.");
                 break;
@@ -25,101 +19,137 @@ public class UI {
             case 3: //Login unsuccesful
                 System.out.println("User ID does not exist.");
                 break;
-            case 4: //Overview
+        }
+    }
+    
+    void startScreen(int screen){
+        switch (screen){
+            case 1: //Overview
+                System.out.println();
                 System.out.println("Please choose your next action:");
                 System.out.println("Type 1 to manage an active project.");
                 System.out.println("Type 2 to create new project.");
                 System.out.println("Type 3 to logout.");
                 break;
-            case 5: //Go to project
+            case 2: //Go to project
                 System.out.println("Enter active project.");
                 break;
-            case 6: //Go to project unsuccesful
+            case 3: //Go to project unsuccesful
                 System.out.println("The given project does not exist.");
                 break;
-            case 7: //Create project
+            case 4: //Create project
                 System.out.println("Please enter the name of the project.");
                 break;
-            case 8: //Create project failed
-                System.out.println("A project with this name already exists.");
-                break;
-            case 9: //Project Creation
+            case 5: //Project Creation successful
                 System.out.println("A project with name " + activeProject + " has been created.");
                 break;
-            case 10: //Project Overview
+            case 6: //Create project failed
+                System.out.println("A project with this name already exists.");
+                break;
+            case 7: //Go to project unsuccesful - userID doesn't have access to the project.
+                System.out.println("You do not have access to the project: " + activeProject);
+                break;
+        }
+    }
+
+    void projectScreen(int screen){
+        switch(screen){
+            case 1: //Project Overview before projectmanager has been assigned
+                System.out.println();
+                System.out.println("Welcome to project: " + activeProject);
+                System.out.println("Please choose your next action:");
+                System.out.println("Type 1 to add a developer to the project.");
+                System.out.println("Type 2 to assign project manager.");
+                System.out.println("Type 3 to return to General Overview.");
+                break;
+            case 2: // Project overview for projectmanagers
+                System.out.println();
+                System.out.println("Welcome to project: " + activeProject);
+                System.out.println("Please choose your next action:");
+                System.out.println("Type 1 to pull project report.");
+                System.out.println("Type 2 to add a deleloper to the project");
+                System.out.println("Type 3 to add a new activity to the project.");
+                System.out.println("Type 4 to manage an active project.");
+                System.out.println("Type 5 to assign new project manager.");
+                System.out.println("Type 6 to return to General Overview.");
+                break; 
+            case 3: // Project Overview for normal developer
+                System.out.println();
                 System.out.println("Welcome to project: " + activeProject);
                 System.out.println("Please choose your next action:");
                 System.out.println("Type 1 to manage an active activity.");
-                System.out.println("Type 2 to assign project manager.");
-                System.out.println("Type 3 to add developer to project.");
-                System.out.println("Type 4 to add an activity to project.");
-                System.out.println("Type 5 to get project report.");
-                System.out.println("Type 6 to return to General Overview.");
+                System.out.println("Type 2 pull activity list for the project.");
+                System.out.println("Type 3 to return to General Overview.");
                 break;
-            case 11: //Go to project unsuccesful - userID doesn't have access to the project.
-                System.out.println("You do not have access to the project: " + activeProject);
+
+            //1 - Pull project report:
+            case 4: //Pull projectreport
+                System.out.println("This is the project report");
                 break;
-            case 12: //Go to activity
-                System.out.println("Please enter the name of the activity.");
+
+            //2 - Add a new developer:
+            case 5: //Add new developer to project)
+                System.out.println("Please enter the userID of the developer you wish to add");
                 break;
-            case 13: //Go to activity failed
-                System.out.println("The given activity does not exist.");
+            case 6: //Given developer is now added to the project 
+                System.out.println("The developer " + userID + " is now a part of " +activeProject+ ".");
                 break;
-            case 14: //Need project manager permission
-                System.out.println("Only the project manager may use this function.");
+            case 7: //Given developer doesn't exist 
+                System.out.println("The developer" + userID + " doesn't exist");
                 break;
-            case 15: //Assign project manager
-                System.out.println("Please enter a user ID to be assigned as project manager.");
+
+            //3 - Add a new project:
+            case 8: //Add new activity to the project
+                System.out.println("Please enter the name of the activity you wish to create");
                 break;
-            case 16: //Assign project manager failed
-                System.out.println("The user ID does not exist or is not a part of this project.");
+            case 9: //Add start date to new activity
+                System.out.println("Please enter the start date of the activity, in the given format 'YYYY-MM-DD'.");
                 break;
-            case 17: //Assign developer to Project
-                System.out.println("Please enter a valid user ID.");
-                break;
-            case 18: //Get projectreport.
-                break;
-            case 19: //Start date of an activity
-                System.out.println("Please enter the start date of the activity in given format 'YYYY-MM-DD'.");
-                break;
-            case 20: //End date of an activity
-                System.out.println("Please enter the end date of the activity in given format 'YYYY-MM-DD'.");
-                break;
-            case 21: //Create activity failed
-                System.out.println("An activity with this name already exists.");
-                break;
-            case 22: //Successful activity Creation
+            case 10: //Add end date to new activity
+                System.out.println("Please enter the end date of the activity, in the given format 'YYYY-MM-DD'.");
+                break; 
+            case 11: //The activity has now been created
                 System.out.println("An activity with name " + activeActivity + " has been created.");
                 break;
-            case 23: //Incorrect dates
+            case 12: //The activity creation have failed
+                System.out.println("An activity with this name already exists.");
+                break;
+            case 13: //Incorrect dates
                 System.out.println("This is not a valid date.");
                 break;
-            case 24: //Projectmanager set
-                System.out.println("The projectmanager of " +activeProject+ " has been set.");
+
+            //4 - Manage an active activity:
+            case 14: //Go to activity
+                System.out.println("Please enter the name of the active you wish to manage.");
                 break;
-            case 25: // Developer not part of project
-                System.out.println("The given developer is not a part of the current project or does not exist.");
+            case 15: //Go to activity unsuccesfully
+                System.out.println("The given activity, " + activeActivity + " doesn't exist.");
                 break;
-            case 26: //Developer added to project
-                System.out.println("The developer is now a part of " +activeProject+ ".");
+
+            //5 - Assign new project manager
+            case 16: //Assign project manager
+                System.out.println("Please enter a user ID to be assigned as project manager.");
                 break;
-            case 27: //Press enter to continue
-                System.out.print("Press enter to continue.\n");
+            case 17: //New projectmanager added
+                System.out.println("The developer " + userID + " is now projectmanager of the project " +activeProject+".");
                 break;
-            case 28: //Go to project
-                System.out.println("Please enter active project.");
-                break;
-            case 29: //Go to project unsuccesfully
-                System.out.println("The given activity does not exist.");
-                break;
-            case 30: //Activity Overview
+            case 18: //Assign project manager failed
+                System.out.println("The user ID doesn't exist or is not a part of this project.");
+                break;      
+        }
+    }
+
+    void activityScreen(int screen){
+        switch(screen){
+            case 1: //Activity Overview
+                System.out.println();
                 System.out.println("You are now managing " +activeProject+ " - " +activeActivity);
                 System.out.println("Press 1 to Log Time.");
                 System.out.println("Press 2 to add a developer to this activity.");
                 System.out.println("Press 3 to change the start date.");
                 System.out.println("Press 4 to change the end date.");
                 System.out.println("Press 5 to return to Project Overview.");
-                break;
+            break;
         }
     }
 
@@ -137,5 +167,13 @@ public class UI {
 
     public String getActivityName(){
         return activeActivity;
+    }
+
+    public void setUserID(String name){
+        userID = name;
+    }
+
+    public String getUserID(){
+        return userID;
     }
 }
